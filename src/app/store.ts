9f5508e737a542bd533d2bd7168a0396356105da
djurasovicvedran <djurasovic.vedran@gmail.com>
2022-07-01 16:50:14 +0200

    https://github.com/djurasovicvedran/Restaurant-reservations.git

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,7 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import reservationSlice from "../features/reservationSlice";
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    reservations: reservationSlice,
+  },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
